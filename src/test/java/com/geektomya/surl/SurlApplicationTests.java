80f052caa3ab1ee8756fef6e0c38e3ee229a9e6e
geektomya <1290341092@qq.com>
2019-12-01 14:46:09 +0800

    :no_mouth:构建项目整体

---
+++ src/test/java/com/geektomya/surl/SurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geektomya.surl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
