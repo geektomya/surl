2f8862d65962c808720e883aa4c14c04abc277c0
geektomya <1290341092@qq.com>
2019-12-02 21:10:36 +0800

    :simple_smile:修改数据库文件

--- doc/surl.sql
+++ doc/surl.sql
@@ -10,7 +10,7 @@ Target Server Type    : MariaDB
 Target Server Version : 100137
 File Encoding         : 65001
 
-Date: 2019-12-02 11:38:40
+Date: 2019-12-02 19:49:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,7 +23,8 @@ CREATE TABLE `url` (
   `id` varchar(255) NOT NULL COMMENT '链接表的字段',
   `real_url` varchar(255) NOT NULL COMMENT '原始的url',
   `short_url` varchar(255) NOT NULL COMMENT '原始url对应的短url',
-  `type` int(10) NOT NULL COMMENT '0表示url是系统自动生成，1表示用户自定义',
+  `type` int(10) unsigned NOT NULL COMMENT '0表示url是系统自动生成，1表示用户自定义',
+  `current` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '用于标识当前记录是否为最后一条',
   PRIMARY KEY (`short_url`),
   UNIQUE KEY `id` (`id`) USING BTREE,
   UNIQUE KEY `short_url` (`short_url`) USING BTREE,
