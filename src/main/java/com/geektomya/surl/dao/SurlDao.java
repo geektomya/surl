d40f8487ecda663ad92f4e66adeea43a8c121db2
yqh74110 <1290341092@qq.com>
2020-01-28 02:31:45 +0800

    添加相应的注释

--- src/main/java/com/geektomya/surl/dao/SurlDao.java
+++ src/main/java/com/geektomya/surl/dao/SurlDao.java
@@ -6,31 +6,51 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * @author yaoqiuhong
+ * @description
+ */
 @Repository
 public interface SurlDao {
 
-    /*
-    * 插入url信息
-    * */
+    /**
+     * 插入url信息
+     * @param urlInfo
+     * @return
+     */
     boolean insertUrlInfo(UrlInfo urlInfo);
-    /*
-    * 更新最新值
-    * */
+
+    /**
+     * 更新最新值
+     * @param id
+     * @return
+     */
     boolean upadteCurrntId(String id);
-    /*
-    * 获取短url对应的原始url信息
-    * */
+
+    /**
+     * 获取短url对应的原始url信息
+     * @param shortUrl
+     * @return
+     */
     UrlVo getRealUrlInfo(String shortUrl);
-    /*
-    * 获取最新的记录
-    * */
+
+    /**
+     * 获取最新的记录
+     * @return
+     */
     UrlInfo getCurrentUrlInfo();
-    /*
-    * 获取长Url对应的短Url
-    * */
+
+    /**
+     * 获取长Url对应的短Url
+     * @param urlVo
+     * @return
+     */
     List<UrlVo> getRealUrlsByShortUrl(UrlVo urlVo);
-    /*
-    * 查找某个id是否被自定义的使用过了
-    * */
+
+    /**
+     * 查找某个id是否被自定义的使用过了
+     * @param id
+     * @return
+     */
     UrlInfo selectIdInUse(String id);
 }
