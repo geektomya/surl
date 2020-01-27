b14f7b1b4344a528ac37ad78f5866c1a89469b6c
geektomya <1290341092@qq.com>
2020-01-27 22:52:18 +0800

    将http升级为https

--- src/main/java/com/geektomya/surl/common/message/Messsage.java
+++ src/main/java/com/geektomya/surl/common/message/Messsage.java
@@ -8,7 +8,7 @@ public class Messsage {
     /*
     * 当短url没有对应的长url的时候跳转到该地址
     * */
-    public static final String INDEX = "http://zhqy.xyz";
+    public static final String INDEX = "https://zhqy.xyz";
     /*
     * 当查询短url的时候无对应的长url，返回该信息
     * */
@@ -16,11 +16,11 @@ public class Messsage {
     /*
     * 返回的短URL的前缀
     * */
-    public static final String PERURL="http://localhost:8087/surl/";
+    public static final String PERURL="https://surl.zhqy.xyz/";
     /*
     * 返回js中的调用url的前缀
     * */
-    public static final String PERINJS="http://localhost:8087/surl/url/";
+    public static final String PERINJS="https://surl.zhqy.xyz/url/";
     /*
    * 操作成功
    * */
